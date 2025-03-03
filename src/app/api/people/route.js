e267453025088d19fba881e1f9c32710162d7ae9
Farothtc <mvkirimi@gmail.com>
2025-03-03 12:26:40 +0300

    feat: backend post adjusted

--- src/app/api/people/route.js
+++ src/app/api/people/route.js
@@ -4,3 +4,26 @@ import people from "@/app/lib/people";
 export async function GET() {
   return NextResponse.json({ success: true, data: people }, { status: 200 });
 }
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+
+    if (!body.name) {
+      return NextResponse.json(
+        { success: false, msg: "Please provide a name" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, person: body.name },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, msg: "Invalid request" },
+      { status: 400 }
+    );
+  }
+}
